58e3f5afb7c06bf6e998a77ef03f9420562793f5
rinmon <rinmon@cn7.jp>
2025-05-23 19:52:35 +0900

    Fix Vite configuration

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import legacy from '@vitejs/plugin-legacy';
+
+export default defineConfig({
+  plugins: [
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    })
+  ],
+  build: {
+    outDir: 'admin/dist',
+    rollupOptions: {
+      input: {
+        'admin-script': 'admin/js/admin-script.js',
+        'admin-style': 'admin/css/admin-style.css'
+      },
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]'
+      }
+    }
+  }
+});
